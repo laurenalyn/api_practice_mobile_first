4fb3602aa994a51dc11f718a0397cd538b23c51c
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-16 20:06:51 -0400

    Completed 1 button that accesses tthe API and gives a new image each time the button is clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,26 @@
-// alert("HI!");
+// alert("HI!");   test if we're linked
 
+const pupButton = document.querySelector('[data-pupButton]')
+const ImageContainer = document.querySelector('[data-imageContainer]');
+
+function getPuppy() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => data.message)
+      .then(addPuppyImage)
+}
+
+function addPuppyImage(modifiedResult) {
+    let pupImage = document.createElement('img');
+    console.log(pupImage);
+    pupImage.setAttribute('src', modifiedResult);
+    console.log(pupImage);
+    ImageContainer.appendChild(pupImage);
+}
+
+
+
+function clickButton() {
+    pupButton.addEventListener('click', getPuppy);
+}
+clickButton();
